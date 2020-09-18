e16bd4b6dd8a2770eb1788332100cadd6e52bfc9
AlexisU32 <uribea360@gmial.com>
2020-09-18 00:42:34 -0500

    add exampleOne folder

---
+++ exampleOne/js/variables.js
@@ -0,0 +1,10 @@
+// Tipos de variables
+ let;
+ const; 
+ var; // Está obsoleta, ya que la variable está declarado globalmente
+
+ let name = 'Alexis';
+ let edad = 22;
+ let estatura = 1.70
+  
+ const fechaEspecial = "20-10-2020"
